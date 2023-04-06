508e6f274ab3d0b70e6c90a940a137d6753c0520
marcoIurato <marcoirato2000@gmail.com>
2023-04-06 03:47:37 +0200

    added: input-subject(to complete)
    Signed-off-by: marcoIurato <marcoirato2000@gmail.com>

--- src/app/pages/search-page/search-page.component.ts
+++ src/app/pages/search-page/search-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Subject, debounceTime, tap } from 'rxjs';
 
 @Component({
   selector: 'app-search-page',
   templateUrl: './search-page.component.html',
   styleUrls: ['./search-page.component.scss']
 })
-export class SearchPageComponent {
+export class SearchPageComponent  {
+
+  text$ = new Subject<string>();
+  printlog(input: Event) {
+    if (this.isEventAndInputEventWithTarget(input)) {
+      console.log(input.target.value);
+      this.text$.next(input.target.value);
+    }
+  }
+  isEventAndInputEventWithTarget(event: Event): event is (InputEvent & { target: HTMLInputElement }) {
+    return event instanceof InputEvent && !!(event.target) && event.target instanceof HTMLInputElement;
+  }
+  valueDebounced$=this.text$.pipe(
+    debounceTime(500),
+    tap((value)=>{
+      console.log(value);
+    })
+  );
 
 }
